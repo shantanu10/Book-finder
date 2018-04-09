7c0ad7aeb487d4620a2e7a81a122f327181144f1
shantanu tripathi <shantanu.akt@gmail.com>
2018-04-09 18:47:25 +0530

    generated all required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookComponent } from './book/book.component';
+import { LibraryComponent } from './library/library.component';
+import { HeaderComponent } from './header/header.component';
+import { PagerComponent } from './pager/pager.component';
+import { SharedComponent } from './shared/shared.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookComponent,
+    LibraryComponent,
+    HeaderComponent,
+    PagerComponent,
+    SharedComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
